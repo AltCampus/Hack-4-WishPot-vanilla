a5b8db38a713a6717a9ca171784ebe5c16823856
vishwa <vishwanathovi@gmail.com>
2018-11-26 22:04:56 +0530

    fixed book section side add transition

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -1,38 +1,49 @@
 // Initializing the checkbox to false at start
 
-document.getElementById("toggle").checked = false;
+document.getElementById("toggle-movie").checked = false;
+document.getElementById("toggle-book").checked = false;
 
- // movieBtn location
+ // movieBtn location 
 
 var movieBtn = document.getElementById("movie-add");
+var bookBtn = document.getElementById("book-add");
+
 
 // Calling the add movie transition event listener
 
 movieBtn.addEventListener('click',checkToggle)
+bookBtn.addEventListener('click',checkToggle)
 
 // Add button transition function
 
 function checkToggle(){
-	if (document.getElementById("toggle").checked){
+
+	// Checking if it is Movie or Book category
+
+	var category = this.getAttribute("id").split("-")[0]; 
+
+
+
+	if (document.getElementById(`toggle-${category}`).checked){
 
 		// Uncheck the checkbox
 
-		document.getElementById("toggle").checked = false;
+		document.getElementById(`toggle-${category}`).checked = false;
 
 		// Rotate back to 0deg
 
-		movieBtn.style.transformOrigin = "55% 50% 0";
-		movieBtn.style.transform = "rotate(0deg)";
-		movieBtn.style.transitionProperty = "transform";
-		movieBtn.style.transitionDuration = "1s";
+		document.getElementById(`${category}-add`).style.transformOrigin = "55% 50% 0";
+		document.getElementById(`${category}-add`).style.transform = "rotate(0deg)";
+		document.getElementById(`${category}-add`).style.transitionProperty = "transform";
+		document.getElementById(`${category}-add`).style.transitionDuration = "1s";
 
 		// Change the color back to dodgerblue
 
-		document.querySelector(".add-btn i").style.color = "dodgerblue";
+		document.querySelector(`#${category}-add i`).style.color = "dodgerblue";
 
 		// Hide the input field
 
-		var movieName = document.getElementById("movie-name")
+		var movieName = document.getElementById(`${category}-name`)
 
 		movieName.style.transitionProperty = "opacity";
 		movieName.style.transitionDuration = "1s";
@@ -43,22 +54,22 @@ function checkToggle(){
 
 		// check the checkbox
 
-		document.getElementById("toggle").checked = true;
+		document.getElementById(`toggle-${category}`).checked = true;
 
 		// Rotate to 315 deg
 
-		movieBtn.style.transformOrigin = "55% 50% 0";
-		movieBtn.style.transform = "rotate(315deg)";
-		movieBtn.style.transitionProperty = "transform";
-		movieBtn.style.transitionDuration = "1s";
+		document.getElementById(`${category}-add`).style.transformOrigin = "55% 50% 0";
+		document.getElementById(`${category}-add`).style.transform = "rotate(315deg)";
+		document.getElementById(`${category}-add`).style.transitionProperty = "transform";
+		document.getElementById(`${category}-add`).style.transitionDuration = "1s";
 
 		// Change the color to tomato
 
-		document.querySelector(".add-btn i").style.color = "red";
+		document.querySelector(`#${category}-add i`).style.color = "red";
 
 		// Unhide the form input
 
-		var movieName = document.getElementById("movie-name")
+		var movieName = document.getElementById(`${category}-name`)
 		
 		movieName.style.transitionProperty = "opacity";
 		movieName.style.transitionDuration = "1s";
